0959da67c353a20b5b4e01bd068c172964e692f5
Filip Mareček <136437185+Vaflex@users.noreply.github.com>
2023-12-01 07:48:34 +0000

    timer

--- kod.js
+++ kod.js
@@ -1,1 +1,12 @@
+function displayDateTime() {
+    const currentDate = new Date();
+    
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    const dateTimeString = currentDate.toLocaleDateString(undefined, options);
+    
+    // Display the date and time in an element with the id "datetime"
+    document.getElementById("timer").textContent = dateTimeString;
+  }
 
+  setInterval(displayDateTime, 1000);
+  displayDateTime();
