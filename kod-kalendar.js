4254cf451bbb861577b403687d5eca38cc1032d9
adamz365 <131455481+adamz365@users.noreply.github.com>
2024-03-11 08:26:09 +0100

    Update kod-kalendar.js

--- kod-kalendar.js
+++ kod-kalendar.js
@@ -31,7 +31,7 @@ function generateCalendar() {
                 if (dayCount === currentDay && i === Math.floor((currentDay + firstDay - 1) / 7)) {
                     cell.classList.add("today");
                 }
-                if (dayCount === 13 || dayCount === 25 || dayCount === 2) {
+                if (dayCount === 11 || dayCount === 25 || dayCount === 6) {
                     cell.classList.add("SnemDay");
                     if (dayCount < currentDay) {
                         cell.classList.add("missed");
